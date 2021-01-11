65c9d7b37c71843a593fca82de03ca1d32e02315
Majalito <synt6313@gmail.com>
2021-01-11 14:06:45 +0800

    'finished_profile'

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,9 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const url = "mongodb://127.0.0.1/missqassociates";
+// const url = "mongodb://127.0.0.1/missqassociates";
+
+const url = "mongodb://msqassociates:sunz8lyoztRAkVIw@cluster0-shard-00-00.uc6o6.mongodb.net:27017,cluster0-shard-00-01.uc6o6.mongodb.net:27017,cluster0-shard-00-02.uc6o6.mongodb.net:27017/MsQAssociates?ssl=true&replicaSet=atlas-7q3496-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 // const url = "mongodb+srv://msqassociates:sunz8lyoztRAkVIw@cluster0.uc6o6.mongodb.net/MsQAssociates?retryWrites=true&w=majority";
 
