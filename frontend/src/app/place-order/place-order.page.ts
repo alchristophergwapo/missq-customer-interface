5607a60414cb82cdba8b10d804bfbe06baba8188
JessaMaeYosores <jessamae.yosores@student.passerellesnumeriques.org>
2021-02-04 16:52:45 +0800

    Added Sweet Alert

--- frontend/src/app/place-order/place-order.page.ts
+++ frontend/src/app/place-order/place-order.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { AppComponent } from '../app.component';
+import Swal from 'sweetalert2';
+import 'sweetalert2/src/sweetalert2.scss';
 
 @Component({
   selector: 'app-place-order',
@@ -113,58 +115,7 @@ export class PlaceOrderPage implements OnInit {
     this.app.dashboard = true;
   }
 
-  // async bookService() {
-  //   const alert = await this.alertCtrl.create({
-  //     header: this.segmentModel,
-  //     // subHeader: "Please fill up the needed information!",
-  //     backdropDismiss: false,
-  //     buttons: [
-  //       {
-  //         text: "Cancel",
-  //         role: "cancel",
-  //         handler: data => { }
-  //       },
-  //       {
-  //         text: "Next",
-  //         handler: data => {
-  //           if (this.segmentModel === "Haircut") {
-  //             this.haircutAlert();
-  //           }
-  //           else {
-  //             this.minuteAssurance();
-  //           }
-  //         }
-  //       }
-  //     ]
-  //   });
-  //   await alert.present();
-  // }
-
-  async inputError() {
-    const alert = await this.alertCtrl.create({
-      cssClass: "my-custom-class",
-      header: "Error!",
-      message: "Please fill up the needed information.",
-      buttons: [
-        {
-          text: "Cancel",
-          role: "cancel",
-          handler: () => {
-          }
-        },
-        {
-          text: "OK",
-          handler: () => {
-            this.minuteAssurance();
-          }
-        }
-      ]
-    });
-
-    await alert.present();
-  }
-
-  async minuteAssurance() {
+  async bookService() {
     const alert = await this.alertCtrl.create({
       header: this.segmentModel,
       subHeader: "Your input must be based on minute not hours. Thank You!",
@@ -199,27 +150,43 @@ export class PlaceOrderPage implements OnInit {
         {
           text: "OK",
           handler: data => {
-            if (data.additional && data.notes) {
-              this.totalCost =
-                this.costOfServie[this.segmentModel] + (data.additional * this.additionalCost[this.segmentModel]);
-              console.log(Math.round(this.totalCost));
-              const params = {
-                service_booking: this.segmentModel,
-                cost: Math.round(this.totalCost),
-                notes: data.notes
-              };
-              let navigationExtras: NavigationExtras = {
-                queryParams: {
-                  bookedData: JSON.stringify(params)
+            Swal.fire({
+              title: 'Do you want to save the changes? <br><br> Total Cost: ₱' + Math.round(this.costOfServie[this.segmentModel] + (data.additional * this.additionalCost[this.segmentModel])),
+              showDenyButton: true,
+              showCancelButton: true,
+              confirmButtonText: `Save`,
+              denyButtonText: `Don't save`,
+            }).then((result) => {
+              /* Read more about isConfirmed, isDenied below */
+              if (result.isConfirmed) {
+                if (data.additional && data.notes) {
+                  Swal.fire('Saved!', '', 'success')
+                  this.totalCost =
+                    this.costOfServie[this.segmentModel] + (data.additional * this.additionalCost[this.segmentModel]);
+                  console.log(Math.round(this.totalCost));
+                  const params = {
+                    service_booking: this.segmentModel,
+                    cost: Math.round(this.totalCost),
+                    notes: data.notes
+                  };
+                  let navigationExtras: NavigationExtras = {
+                    queryParams: {
+                      bookedData: JSON.stringify(params)
+                    }
+                  };
+                  this.router.navigate(
+                    ["place-order/location-select"],
+                    navigationExtras
+                  );
+
+                } else {
+                  this.inputError();
                 }
-              };
-              this.router.navigate(
-                ["place-order/location-select"],
-                navigationExtras
-              );
-            } else {
-              this.inputError();
-            }
+              } else if (result.isDenied) {
+                Swal.fire('Changes are not saved', '', 'info')
+              }
+            })
+
           }
         }
       ]
@@ -254,26 +221,41 @@ export class PlaceOrderPage implements OnInit {
         {
           text: "OK",
           handler: (data) => {
-            if (data.head && data.notes) {
-              this.totalCost = this.costOfServie.Haircut * data.head;
-              console.log(Math.round(this.totalCost));
-              const params = {
-                service_booking: this.segmentModel,
-                cost: Math.round(this.totalCost),
-                notes: data.notes
-              };
-              let navigationExtras: NavigationExtras = {
-                queryParams: {
-                  bookedData: JSON.stringify(params)
+            Swal.fire({
+              title: 'Do you want to save the changes? <br><br> Total Cost: ₱' + Math.round(this.costOfServie.Haircut * data.head),
+              showDenyButton: true,
+              showCancelButton: true,
+              confirmButtonText: `Save`,
+              denyButtonText: `Don't save`,
+            }).then((result) => {
+              if (result.isConfirmed) {
+                /* Read more about isConfirmed, isDenied below */
+                if (data.head && data.notes) {
+                  Swal.fire('Saved!', '', 'success')
+                  this.totalCost = this.costOfServie.Haircut * data.head;
+                  console.log(Math.round(this.totalCost));
+                  const params = {
+                    service_booking: this.segmentModel,
+                    cost: Math.round(this.totalCost),
+                    notes: data.notes
+                  };
+                  let navigationExtras: NavigationExtras = {
+                    queryParams: {
+                      bookedData: JSON.stringify(params)
+                    }
+                  };
+                  this.router.navigate(
+                    ["place-order/location-select"],
+                    navigationExtras
+                  );
+                } else {
+                  this.inputErrorHaircut();
                 }
-              };
-              this.router.navigate(
-                ["place-order/location-select"],
-                navigationExtras
-              );
-            } else {
-              this.inputError();
-            }
+              } else if (result.isDenied) {
+                Swal.fire('Changes are not saved', '', 'info')
+              }
+            })
+
           }
         }
       ]
@@ -282,6 +264,53 @@ export class PlaceOrderPage implements OnInit {
     await alert.present();
   }
 
+  async inputError() {
+    const alert = await this.alertCtrl.create({
+      cssClass: "my-custom-class",
+      header: "Error!",
+      message: "Please fill up the needed information.",
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          handler: () => {
+          }
+        },
+        {
+          text: "OK",
+          handler: () => {
+            this.bookService();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async inputErrorHaircut() {
+    const alert = await this.alertCtrl.create({
+      cssClass: "my-custom-class",
+      header: "Error!",
+      message: "Please fill up the needed information.",
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          handler: () => {
+          }
+        },
+        {
+          text: "OK",
+          handler: () => {
+            this.haircutAlert();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
   segmentChanged(event) {
     this.segmentModel = event;
