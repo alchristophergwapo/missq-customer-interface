1102147a5710bf7a66075f5b334599fe4e9d46d6
JessaMaeYosores <jessamae.yosores@student.passerellesnumeriques.org>
2021-02-04 15:17:54 +0800

    Computed the total cost

--- frontend/src/app/place-order/place-order.page.ts
+++ frontend/src/app/place-order/place-order.page.ts
@@ -80,6 +80,13 @@ export class PlaceOrderPage implements OnInit {
     Haircut: 250
   };
 
+  additionalCost: any = {
+    Nanny: 1.66666,
+    Housekeeping: 1.66666,
+    TutorCumNanny: 2.5,
+    MassageTherapist: 5
+  };
+
   description: any = {
     Nanny: 'Hours of service',
     Housekeeping: 'Hours of service',
@@ -106,16 +113,74 @@ export class PlaceOrderPage implements OnInit {
     this.app.dashboard = true;
   }
 
-  async bookService() {
+  // async bookService() {
+  //   const alert = await this.alertCtrl.create({
+  //     header: this.segmentModel,
+  //     // subHeader: "Please fill up the needed information!",
+  //     backdropDismiss: false,
+  //     buttons: [
+  //       {
+  //         text: "Cancel",
+  //         role: "cancel",
+  //         handler: data => { }
+  //       },
+  //       {
+  //         text: "Next",
+  //         handler: data => {
+  //           if (this.segmentModel === "Haircut") {
+  //             this.haircutAlert();
+  //           }
+  //           else {
+  //             this.minuteAssurance();
+  //           }
+  //         }
+  //       }
+  //     ]
+  //   });
+  //   await alert.present();
+  // }
+
+  async inputError() {
+    const alert = await this.alertCtrl.create({
+      cssClass: "my-custom-class",
+      header: "Error!",
+      message: "Please fill up the needed information.",
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          handler: () => {
+          }
+        },
+        {
+          text: "OK",
+          handler: () => {
+            this.minuteAssurance();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async minuteAssurance() {
     const alert = await this.alertCtrl.create({
       header: this.segmentModel,
-      subHeader: "Please fill up the needed information!",
+      subHeader: "Your input must be based on minute not hours. Thank You!",
       backdropDismiss: false,
       inputs: [
+        // {
+        //   name: "hours",
+        //   placeholder: this.description[this.segmentModel],
+        //   value: 1 + "hour and" + 30 + "minutes",
+        //   type: "number"
+        // },
         {
-          name: "hours",
-          placeholder: this.description[this.segmentModel],
-          type: "number"
+          name: "additional",
+          placeholder: "Additional Minute/s",
+          type: "number",
+
         },
         {
           name: "notes",
@@ -127,17 +192,20 @@ export class PlaceOrderPage implements OnInit {
         {
           text: "Cancel",
           role: "cancel",
-          handler: data => { }
+          handler: data => {
+            // this.bookService();
+          }
         },
         {
-          text: "Next",
+          text: "OK",
           handler: data => {
-            if (data.hours && data.notes) {
+            if (data.additional && data.notes) {
               this.totalCost =
-                this.costOfServie[this.segmentModel] * data.hours;
+                this.costOfServie[this.segmentModel] + (data.additional * this.additionalCost[this.segmentModel]);
+              console.log(Math.round(this.totalCost));
               const params = {
                 service_booking: this.segmentModel,
-                cost: this.totalCost,
+                cost: Math.round(this.totalCost),
                 notes: data.notes
               };
               let navigationExtras: NavigationExtras = {
@@ -159,16 +227,53 @@ export class PlaceOrderPage implements OnInit {
     await alert.present();
   }
 
-  async inputError() {
+  async haircutAlert() {
     const alert = await this.alertCtrl.create({
       cssClass: "my-custom-class",
-      header: "Error!",
+      header: "Reminder!",
       message: "Please fill up the needed information.",
+      inputs: [
+        {
+          name: "head",
+          placeholder: this.description.Haircut,
+          type: "number",
+        },
+        {
+          name: "notes",
+          placeholder: "Notes",
+          type: "textarea",
+        }
+      ],
       buttons: [
         {
-          text: "OK",
+          text: "Cancel",
+          role: "cancel",
           handler: () => {
-            this.bookService();
+          }
+        },
+        {
+          text: "OK",
+          handler: (data) => {
+            if (data.head && data.notes) {
+              this.totalCost = this.costOfServie.Haircut * data.head;
+              console.log(Math.round(this.totalCost));
+              const params = {
+                service_booking: this.segmentModel,
+                cost: Math.round(this.totalCost),
+                notes: data.notes
+              };
+              let navigationExtras: NavigationExtras = {
+                queryParams: {
+                  bookedData: JSON.stringify(params)
+                }
+              };
+              this.router.navigate(
+                ["place-order/location-select"],
+                navigationExtras
+              );
+            } else {
+              this.inputError();
+            }
           }
         }
       ]
@@ -176,6 +281,7 @@ export class PlaceOrderPage implements OnInit {
 
     await alert.present();
   }
+
 
   segmentChanged(event) {
     this.segmentModel = event;
